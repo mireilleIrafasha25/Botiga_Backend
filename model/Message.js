7da150545469e5b6d058d9c33a27113f11bd89b7
mireilleIrafasha25 <mireilleirafasha@gmail.com>
2025-02-24 17:06:42 +0200

    small

--- model/Message.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const messageSchema = new mongoose.Schema({
-  sender: String, // "user" or "bot"
-  text: String,
-  timestamp: { type: Date, default: Date.now },
-});
-
-export default mongoose.model("Message", messageSchema);
--- route/ChatBotRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { handleChat } from "../controller/ChatbotController.js";
-
-const router = express.Router();
-
-router.post("/new", handleChat);
-
-export default router;
