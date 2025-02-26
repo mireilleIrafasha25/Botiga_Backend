e98300ae6e6b531dc1d070150450a8254b4062b9
mireilleIrafasha25 <mireilleirafasha@gmail.com>
2025-02-26 15:12:28 +0200

    fnjn

---
+++ controller/BlogController.js
@@ -0,0 +1,151 @@
+import BlogModel from "../model/BlogModel";
+import asyncWrapper from "../middleware/async.js";
+import { validationResult } from "express-validator";
+import { NotFoundError } from "../error/notfoundError.js";
+import dotenv from "dotenv"
+import cloudinary from "../utils/cloudinary.js"
+import path from "path";
+dotenv.config();
+export const TestBlog=(req,res,next)=>
+{
+    res.status(200).json({message:'Welcome to Blog'});
+}
+export const AddBlog = asyncWrapper(async (req, res, next) => {
+    try {
+    const errors= validationResult(req);
+        if(!errors.isEmpty())
+        {
+            console.log(errors.array());
+             next(new BadRequestError(errors.array()[0].msg))
+        }
+      if (!req.file) {
+        return res.status(400).json({ error: "Image file is required" });
+      }
+  
+      const filePath = path.resolve(req.file.path); // Convert to absolute path
+  
+      const result = await cloudinary.uploader.upload(filePath, {
+        use_filename: true,
+        unique_filename: false,
+        overwrite: true,
+      });
+  
+  
+      if (!result || !result.url) {
+        throw new Error("Failed to upload image to Cloudinary");
+      }
+  
+      const blog = new BlogModel({
+        description: req.body.description,
+        date:req.body.date,
+        image: {
+          url: result.url,
+        },
+      });
+  
+      const savedBlog = await blog.save();
+  
+      return res.status(201).json({
+        message: "Product created successfully",
+        product: savedBlog,
+      });
+    } catch (error) {
+      console.error("Error adding product:", error);
+      return res.status(500).json({ error: error.message });
+    }
+  });
+  export const GetProducts=asyncWrapper(async(req,res,next)=>
+    {
+        const products=await ProductModel.find();
+        if(!products)
+        {
+            return next(new NotFoundError('No products found'));
+        }
+        else
+        {
+            res.status(200).json({
+                size: products.length,
+                products});
+        }
+      
+    })
+    
+    export const UpdateProduct=asyncWrapper(async(req, res, next)=>
+    {
+        const productId=req.params.productId;
+        const updatedData=req.body;
+        const errors=validationResult(req);
+        if(!errors.isEmpty())
+        {
+            console.log(errors.array());
+            return next(new BadRequestError(errors.array()[0].msg));
+        }
+       const updatedProduct=await ProductModel.findOneAndUpdate(
+        {productId: productId},
+         updatedData, 
+        {new:true,runValidators:true}
+       );
+       if(!updatedProduct)
+       {
+        return res.status(404).json({
+            message:'Product not found'
+        });
+       }
+       res.status(200).json({
+        message:'Product updated successfully',
+        product:updatedProduct
+       })
+    })
+    
+    export const DeleteProduct=asyncWrapper(async(req,res,next)=>
+    {
+        const productId=req.params.id;
+        const deletedProduct=await ProductModel.findByIdAndDelete(productId);
+        if(!deletedProduct)
+        {
+            return res.status(404).json({
+                message:'Product not found'
+            });
+        }
+        res.status(200).json({
+            message:'Product deleted successfully',
+            product:deletedProduct
+        })
+    })
+    
+    export const GetProductById=asyncWrapper(async(req,res,next)=>
+    {
+        const productId = Number(req.params.productId); // Convert to Number
+        if (isNaN(productId)) {
+          return res.status(400).json({ message: "Invalid productId" });
+        }
+    
+        const product = await ProductModel.findOne({ productId });
+        if(!product)
+        {
+            return next(new NotFoundError('Product not found'));
+        }
+        else
+        {
+            res.status(200).json({
+                product
+            })
+        }
+    })
+    
+    export const GetProductsByCategory=asyncWrapper(async(req,res,next)=>
+    {
+        const category=req.params.category;
+        const products=await ProductModel.find({category:category});
+        if(!products)
+        {
+            return next(new NotFoundError('No products found in this category'));
+        }
+        else
+        {
+            res.status(200).json({
+                size: products.length,
+                products
+            })
+        }
+    })
---
+++ model/BlogModel.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const schema = mongoose.Schema;
+const BlogSchema = new schema({
+  date:{type:Date, required:[true,"Please enter date"]},
+  description: { type: String, required: [true, "Please enter the product description"] },
+  image:{
+    url:{
+    type: String,
+  }},
+  
+});
+
+
+//Blog model and entity name called Blog
+const BlogModel = mongoose.model("Blog", BlogSchema);
+export default BlogModel;
