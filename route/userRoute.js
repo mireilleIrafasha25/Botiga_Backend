1915a371ca6523814c7c822e0f1c2b6eb2a82b4c
mireilleIrafasha25 <mireilleirafasha@gmail.com>
2025-03-01 06:33:06 +0200

    authorize

--- route/userRoute.js
+++ route/userRoute.js
@@ -2,12 +2,12 @@
 import {SignIn,SignUp,ResetPassword,ForgotPassword,Validateopt,Logout,test, getAllusers,updateUser,findUserByName,deleteUser} from '../controller/usercontroller.js';
 import express from 'express';
 import { signUpValidation,signInValidation,otpValidation,resetPasswordValidation,forgotpasswordValidation, } from '../utils/validation.js';
-import {authenticateToken} from "../middleware/authethicateToken.js"
+import {authenticateToken,authorize} from "../middleware/authethicateToken.js"
 const route= express.Router();
 route.get("/Test",test)
 route.post('/signup',signUpValidation,SignUp)
 route.post('/signin',signInValidation,SignIn)
-route.get('/listAll',getAllusers)
+route.get('/listAll',authenticateToken,authorize("admin"),getAllusers)
 route.post('/resetpassword',resetPasswordValidation,ResetPassword)
 route.post('/forgotpassword',forgotpasswordValidation,ForgotPassword)
 route.post('/verify',otpValidation,Validateopt)
